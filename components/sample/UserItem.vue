66aa000f92d17e7bf20ca9c1bb10ed0703414b0f
ymatsuo777 <ymatsuo@itfor.co.jp>
2021-03-30 10:35:58 +0900

    package.jsonの修正&サンプルの追加

---
+++ components/sample/UserItem.vue
@@ -0,0 +1,28 @@
+<template>
+  <div @click="clickData">
+    <span>{{ no }}</span>
+    <span>{{ name }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserItem',
+  // コンポーネントの外側から渡されるデータをpropsという
+  props: {
+    no: {
+      type: Number, // 渡されるデータの型
+      default: 0 // デフォルト値
+    },
+    name: {
+      type: String,
+      default: ''
+    }
+  },
+  methods: {
+    clickData() {
+      alert(`${this.name} がクリックされました`)
+    }
+  }
+}
+</script>
---
+++ pages/sample/index.vue
@@ -0,0 +1,81 @@
+<template>
+  <div>
+    <div class="title">
+      ユーザー一覧
+    </div>
+    <!-- dataで定義した変数の内容を表示 -->
+    {{ loginUser }}
+
+    <!-- dataで定義したusers（配列）の数分ループを回す => v-forの使用 -->
+    <!-- keyにはループによって生成されたHTMLに一意のキーを設定する必要がある -->
+    <!-- これは変数の内容が変更された場合等に再レンダリングが行われる場合のターゲットの識別に利用され -->
+    <!-- 付けないとパフォーマンス上の影響が出る場合がある -->
+    <div v-for="u in users" :key="u.no" class="list">
+      <!-- UserItemコンポーネントの利用 -->
+      <!-- usersのnoとnameをコンポーネントに渡している -->
+      <UserItem :no="u.no" :name="u.name" />
+    </div>
+
+    <!-- @click ･･･ クリックした場合の処理を設定する（その他 @blur, @dbclickなどなど） -->
+    <button @click="changeData">
+      ボタン
+    </button>
+  </div>
+</template>
+
+<script>
+// この画面で使用するコンポーネントなどをインポートする
+import UserItem from '@/components/sample/UserItem'
+
+export default {
+  // この画面で使用するコンポーネントを設定する
+  components: {
+    UserItem
+  },
+  // この画面で使用する変数を定義する
+  data() {
+    return {
+      loginUser: '',
+      users: []
+    }
+  },
+  // ライフサイクルフック
+  // https://jp.vuejs.org/v2/guide/instance.html#ライフサイクルダイアグラム
+  // https://ja.nuxtjs.org/docs/2.x/concepts/nuxt-lifecycle/
+  // created ･･･ インスタンスが生成された後にコードを実行
+  created() {
+    this.loginUser = 'サンプルユーザー'
+
+    this.users.push({
+      no: 1,
+      name: '田中'
+    })
+    this.users.push({
+      no: 2,
+      name: '山田'
+    })
+  },
+  // この画面で使用する処理を定義する
+  methods: {
+    // 関数
+    changeData() {
+      const idx = this.users.length
+      this.users.push({
+        no: idx + 1,
+        name: '追加されたデータ'
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+/* デザインの定義 普通のCSSが使用できる */
+.title {
+  font-weight: bold;
+}
+
+.list {
+  margin: 10px;
+}
+</style>
